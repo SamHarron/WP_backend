c5b9620963956b0dcc47556b98f26166c454f2a8
SamHarron <harronsam@gmail.com>
2021-09-12 20:49:21 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Workout.create(title:"Monday Sculpt", date: "2021-09-06", message: "Chest, Triceps, Abs")
+Workout.create(title:"Tuesday Motion", date: "2021-09-07", message: "#Legs")
+Workout.create(title:"Wednesday Super", date: "2021-09-08", message: "All Arms")
+Workout.create(title:"Thursday Tough", date: "2021-09-09", message: "Shoulders, Back")
+Workout.create(title:"Friday Pick", date: "2021-09-10", message: "Anything, Get Big")
+Workout.create(title:"Saturday Jog", date: "2021-09-11", message: "Run")
+
+puts "The Seeds Have Been Sown"
